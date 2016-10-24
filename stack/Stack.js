a65ed36354f694bcbda0ac41cc9ea3c79de3b8a7
telmanagababov <telman.agababov@gmail.com>
2016-10-25 02:47:28 +0300

    add stack structure

---
+++ stack/Stack.js
@@ -0,0 +1,37 @@
+function Stack() {
+
+    let data = [];
+
+    function push(value) {
+        data.push(value);
+    }
+
+    function pop() {
+        return data.pop();
+    }
+
+    function peek() {
+        return data[data.length -1];
+    }
+
+    function getSize() {
+        return data.length;
+    }
+
+    function isEmpty() {
+        return data.length === 0;
+    }
+
+    function clear() {
+        data = [];
+    }
+
+    return {
+        push: push,
+        pop: pop,
+        peek: peek,
+        getSize: getSize,
+        isEmpty: isEmpty,
+        clear: clear
+    }
+}
